d92c65d062a0a7791961981bd4610cd65e0d0551
Cloudy Coder <kushalmaharjan472@gmail.com>
2025-12-29 13:27:00 +0545

    New Features

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ $(document).ready(function () {
         $('.navbar').toggleClass('active');    // show/hide menu
     });
 });
+window.addEventListener("load", function () {
+    const loader = document.getElementById("page-loader");
+    loader.style.opacity = "0";
+    loader.style.pointerEvents = "none";
+
+    setTimeout(() => {
+        loader.style.display = "none";
+    }, 500);
+}, 9000);
